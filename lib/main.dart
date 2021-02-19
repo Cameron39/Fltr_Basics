6dfb6f8425a338d2b82cd86f6499ed68f6fb89cb
Cameron39 <cameron.39@gmail.com>
2021-02-19 11:41:33 -0600

    Scaffolding background with Image from Web/Network

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey[850],
         appBar: AppBar(
           title: Text("I am Rich"),
           backgroundColor: Colors.orange[800],
         ),
-        backgroundColor: Colors.grey[850],
+        body: Image(
+          image: NetworkImage(
+              'https://s7d2.scene7.com/is/image/TWCNews/snowflake-formatted-snow-03222020jpg'),
+        ),
       ),
     ),
   );
